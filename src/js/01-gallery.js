f1e28cbc81098d312f96488750e81dc5a4a43e85
SlavaVG <shlikhten@gmail.com>
2022-01-30 21:36:43 +0200

    WH8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+let galEl = document.querySelector('.gallery');
+
+const picsList = galleryItems
+  .map(
+    pic => `<a class="gallery__item" href="${pic.original}">
+                <img class="gallery__image" src=${pic.preview} alt=${pic.description} title=${pic.description} />
+            </a>`, ).join('');
+
+galEl.insertAdjacentHTML('beforeend', picsList);
+
+const lengthEL = document.getElementsByClassName('gallery__item').length;
+
+for (let i = 0; i < lengthEL; i +=1) {
+  document.getElementsByClassName('gallery__item')[i].addEventListener('click', function (event) {
+    event.preventDefault();
+  });
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'title',
+  animationSpeed: 250
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash/throttle';
 
+const _ = require('lodash');
+const formInput = document.querySelector('.feedback-form');
+
+let userInput = { email: '', password: '' };
+
+if (localStorage.getItem('feedback-form-state')) {
+  userInput = JSON.parse(localStorage.getItem('feedback-form-state'));
+}
+
+if (userInput.email.length > 0 || userInput.password.length > 0) {
+  formInput.elements.namedItem('email').value = userInput.email;
+  formInput.elements.namedItem('message').value = userInput.password;
+}
+
+function updateValue(e) {
+  userInput.email = formInput.elements.namedItem('email').value;
+  userInput.password = formInput.elements.namedItem('message').value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(userInput));
+}
+
+var throttled = _.throttle(updateValue, 500);
+
+formInput.addEventListener('input', throttled);
+
+function refreshFields(e) {
+  e.preventDefault();
+  console.log(`email: ${userInput.email}, password: ${userInput.password}`);
+  formInput.elements.namedItem('email').value = '';
+  formInput.elements.namedItem('message').value = '';
+  localStorage.clear();
+}
+
+formInput.addEventListener('submit', refreshFields);
